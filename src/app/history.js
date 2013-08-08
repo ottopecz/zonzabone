225f4eee09ce58774b9ec761f6980c6d40ab6d48
Thomas Grainger <thomasgrainger@graingert.co.uk>
2013-08-08 10:36:08 +0100

    70e142cefc Revert "fix jshint errors"
    This reverts commit 9f712aeec475fbf1ce977e8e71c250431a7f3138.

--- src/app/history.js
+++ src/app/history.js
@@ -1,36 +1,37 @@
 /*global define*/
-
-define(['jquery'], function ($) {
+define(function (require) {
     "use strict";
 
-    console.log('history.js');
+    var $ = require('jquery');
 
-    var that = {},
+	console.log('history.js');
 
-    _popstateHandler = null,
+	var that = {},
 
-    _addeventListener = function (event, popstateHandler) {
-        window.addEventListener(event, popstateHandler);
-    };
+	_popstateHandler = null,
 
-    /**
-     * Changes updates browser location bar and fires callback
-     */
-    that.push = function (url, state, popstateHandler) {
-        window.history.pushState(state, null, url);
-        _popstateHandler = popstateHandler;
-    };
+	_addeventListener = function(event, popstateHandler) {
+		window.addEventListener(event, popstateHandler);
+	};
 
-    /**
-     * Initializes the history module
-     * @returns {history module}
-     */
-    that.init = function (options) {
-        _addeventListener("popstate", function (e) {
-            _popstateHandler(e);
-        }.bind(that));
-        return this;
-    };
+	/**
+	 * Changes updates browser location bar and fires callback
+	 */
+	that.push = function(url, state, popstateHandler){
+		window.history.pushState(state, null, url);
+    	_popstateHandler = popstateHandler;
+	};
 
-    return that;
+	/**
+	 * Initializes the history module
+	 * @returns {history module}
+	 */
+	that.init = function(options) {
+		_addeventListener("popstate", (function(e) {
+			_popstateHandler(e);
+		}).bind(that));
+		return this;
+	};
+
+	return that;
 });
