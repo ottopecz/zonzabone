10f55376f43f4b3ce6f24bdc8efe91663940c212
Otto Pecz <otto.pecz@hogarthww.com>
2013-10-02 15:42:57 +0100

    QUnit should load before any test module gets loaded

--- test/main.js
+++ test/main.js
@@ -1,3 +1,4 @@
+/*global require, QUnit*/
 require.config({
     'baseUrl'       : '../',
     'waitSeconds'   : 5,
@@ -9,22 +10,18 @@ require.config({
         'mustache'  : 'lib/mustache',
         'sinon'     : 'lib/sinon',
         'mockjax'   : 'lib/jquery/jquery.mockjax',
-        'polyfills' : 'lib/polyfills',
-        'qunit'     : 'lib/qunit/qunit'
+        'polyfills' : 'lib/polyfills'
     },
     'shim' : {
-        'qunit'     : { deps : ['jquery'] },
         'mockjax'   : { deps : ['jquery'] }
     },
     'deps' : [
         'jquery',
-        'qunit',
         'test/collection',
         'test/utils'
     ],
     'callback'      : function () {
-
         $.ajaxSetup({ contentType: 'application/json', 'async': false });
-        QUnit.load();
+        QUnit.start();
     }
 });
