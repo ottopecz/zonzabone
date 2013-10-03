96efd289c3c6ec423e314398d49490a82dfe3c6f
Phillip Shum <phillipshum@hogarthww.com>
2013-10-03 09:51:01 +0100

    model tests

--- test/model.js
+++ test/model.js
@@ -16,6 +16,21 @@ define(function (require) {
             .THEN(eventIsTriggered, 'change:colour');
     });
 
+    test('set', function () {
+
+        bdd.GIVEN(modelInstanceWithEventHandler, 'change:colour', changeHandler)
+            .WHEN(setPropertyWith, 'size', 'big')
+            .THEN(eventIsNotTriggered, 'change:colour');
+    });
+
+    test('set', function () {
+
+        bdd.GIVEN(modelInstanceWithEventHandler, 'change:colour', changeHandler)
+            .WHEN(handlerIsRemoved, 'change:colour')
+            .AND(setPropertyWith, 'colour', 'red')
+            .THEN(eventIsNotTriggered, 'change:colour');
+    });
+
     var modelInstance = function () {
 
             return model();
@@ -26,9 +41,29 @@ define(function (require) {
             return modelInstance().on(e, handler);
         },
 
+        handlerIsRemoved = function (e) {
+
+            bdd.given.off(e);
+        },
+
         changeHandler = function () {
 
+            console.log('changeHandler()');
+
             bdd.given.set('eventTriggered', true);
+        },
+
+        eventIsNotTriggered = function (e) {
+
+            stop();
+
+            setTimeout(function () {
+
+                notStrictEqual(bdd.given.get('eventTriggered'), true, 'Event is not triggered on setting of irrelevant property');
+
+                start();
+
+            }, 100);
         },
 
         eventIsTriggered = function (e) {
@@ -37,7 +72,7 @@ define(function (require) {
 
             setTimeout(function () {
 
-                equal(bdd.given.get('eventTriggered'), true, 'Event is triggered on setting of property');
+                strictEqual(bdd.given.get('eventTriggered'), true, 'Event is triggered on setting of property');
 
                 start();
 
